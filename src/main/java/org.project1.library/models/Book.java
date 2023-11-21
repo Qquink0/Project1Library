2e97a027766878c56bb583851e081b7ecc8c0b66
Ruslan <overbah98@gmail.com>
2023-11-21 20:18:59 +0700

    4

--- src/main/java/org.project1.library/models/Book.java
+++ src/main/java/org.project1.library/models/Book.java
@@ -2,50 +2,50 @@ package org.project1.library.models;
 
 public class Book {
 
-    private int bookId;
-    private String nameBook;
-    private String author;
-    private int year;
+    private int book_id;
+    private String book_name;
+    private String book_author;
+    private int book_year;
 
     public Book() {
     }
 
-    public Book(int bookId, String nameBook, String author, int year) {
-        this.bookId = bookId;
-        this.nameBook = nameBook;
-        this.author = author;
-        this.year = year;
+    public Book(int book_id, String book_name, String book_author, int book_year) {
+        this.book_id = book_id;
+        this.book_name = book_name;
+        this.book_author = book_author;
+        this.book_year = book_year;
     }
 
     public int getBookId() {
-        return bookId;
+        return book_id;
     }
 
     public void setBookId(int bookId) {
-        this.bookId = bookId;
+        this.book_id = bookId;
     }
 
-    public String getNameBook() {
-        return nameBook;
+    public String getBook_name() {
+        return book_name;
     }
 
-    public void setNameBook(String nameBook) {
-        this.nameBook = nameBook;
+    public void setBook_name(String book_name) {
+        this.book_name = book_name;
     }
 
-    public String getAuthor() {
-        return author;
+    public String getBook_author() {
+        return book_author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setBook_author(String book_author) {
+        this.book_author = book_author;
     }
 
-    public int getYear() {
-        return year;
+    public int getBook_year() {
+        return book_year;
     }
 
-    public void setYear(int year) {
-        this.year = year;
+    public void setBook_year(int book_year) {
+        this.book_year = book_year;
     }
 }
--- src/main/java/org.project1.library/models/Person.java
+++ src/main/java/org.project1.library/models/Person.java
@@ -2,40 +2,40 @@ package org.project1.library.models;
 
 public class Person {
 
-    private int personId;
-    private String name;
-    private int year;
+    private int person_id;
+    private String person_name;
+    private int person_year;
 
     public Person() {
     }
 
-    public Person(int personId,String name, int year) {
-        this.personId = personId;
-        this.name = name;
-        this.year = year;
+    public Person(int person_id, String person_name, int person_year) {
+        this.person_id = person_id;
+        this.person_name = person_name;
+        this.person_year = person_year;
     }
 
-    public int getPersonId() {
-        return personId;
+    public int getPerson_id() {
+        return person_id;
     }
 
-    public void setPersonId(int personId) {
-        this.personId = personId;
+    public void setPerson_id(int person_id) {
+        this.person_id = person_id;
     }
 
-    public String getName() {
-        return name;
+    public String getPerson_name() {
+        return person_name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPerson_name(String person_name) {
+        this.person_name = person_name;
     }
 
-    public int getYear() {
-        return year;
+    public int getPerson_year() {
+        return person_year;
     }
 
-    public void setYear(int year) {
-        this.year = year;
+    public void setPerson_year(int person_year) {
+        this.person_year = person_year;
     }
 }
